df22943573a0e90dc896eb7f505ecc1e17cba9a3
titiushadow <mesaque1212@gmail.com>
2023-03-16 21:09:51 -0300

    Finalizando array

---
+++ Aula_34_Array/js/script.js
@@ -0,0 +1,12 @@
+const animais = ["mula", false]
+console.log(animais)
+
+animais[0] = "cachorro";
+animais[1] = "gato";
+animais[2] = 7
+animais[5] = "anta"
+
+
+console.log(animais)
+
+console.log(animais[5])
---
+++ Aula_35_trabalhando_array/js/script.js
@@ -0,0 +1,19 @@
+const arr = ["Abacaxi", "Melão", "Banana", "Melancia", "Goiaba", "Abacate", "Maça", "Laranja"]
+console.log(arr)
+
+// Adicionar Elementos no array
+
+//  Push
+
+arr.push("Pera");
+console.log(arr)
+
+// Unshift
+arr.unshift("pessego")
+console.log(arr)
+
+// Remover Elementos
+// POP
+const removeElement = arr.pop()
+console.log(arr)
+console.log(removeElement)
